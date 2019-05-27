0cdf5f7a682783caea981961405601822c5b7beb
selimcan <ozdemirselimcan@gmail.com>
2019-05-27 12:46:07 +0300

    Mübeccel Mübeccel ben ben olayım da seni hiç anlamayayım ha

---
+++ src/Mubeccel/Apps/home/views/contact.php
@@ -0,0 +1,18 @@
+<h2>İletişim</h2>
+<hr>
+<p>
+    Selim Can Özdemir
+</p>
+
+<p>
+    <span class="font-weight-bold">Telegram :</span> <a target="__blank" href="https://t.me/selimcan">@selimcan</a>
+</p>
+<p>
+    <span class="font-weight-bold">Mail :</span> <a href="mailto:ozdemirselimcan@gmail.com">ozdemirselimcan@gmail.com</a>
+</p>
+<p>
+    <span class="font-weight-bold">Github :</span> <a href="https://github.com/selimcanozdemir">selimcanozdemir</a>
+</p>
+<p>
+    <span class="font-weight-bold">Web Sitesi :</span> <a href="https://selimcan.com">selimcan.com</a>
+</p>
